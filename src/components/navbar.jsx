34cd67ef30ae35d0ec93b3bf59b2d8c7d406100b
aryensantos <aryensantos@gmail.com>
2019-05-08 18:21:12 +0800

    last last

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,7 +10,7 @@ class Header extends Component {
                 <a className="navbar-brand" href="/"><i class="fas fa-link"></i> BeginConnect <i class="fas fa-link"></i></a>
                 <NavLink to="/accounts"><i class="fas fa-user-circle"></i> Accounts</NavLink>
                 <NavLink to="/openaccount"><i class="fas fa-user"></i> Open Account</NavLink>
-                <NavLink to="/transfermoney"><i class="fas fa-money-bill-wave"></i> Transfer Money</NavLink>
+                {/* <NavLink to="/transfermoney"><i class="fas fa-money-bill-wave"></i> Transfer Money</NavLink> */}
                 <NavLink to="/merchants"><i class="fas fa-business-time"></i> Merchant</NavLink>
                 <NavLink to="/addmerchant"><i class="fas fa-plus"></i> Add Merchant</NavLink>
                 <NavLink to="/transactions"><i class="fas fa-tasks"></i> Transactions</NavLink>
